fb765b3c8ba36343419c5dc22d59f9536c0a2a3a
eleman <eleman@webloomsolutions.com>
2021-06-12 16:15:52 +0300

    Initial Commit

---
+++ filmapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  filmapp
+//
+//  Created by MacBook Pro on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
